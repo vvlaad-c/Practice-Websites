f788aadfcda1ac028d89b8a7e6b9180609437428
vvlaad-c <maiestrau@gmail.com>
2024-02-10 11:18:26 +0200

    Added views for all months

--- Django_course/monthly_challenges/challenges/urls.py
+++ Django_course/monthly_challenges/challenges/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:month>/', views.monthly_challenge)
+    path('<int:month>/', views.monthly_challenge_int),
+    path('<str:month>/', views.monthly_challenge),
 ]
  
--- Django_course/monthly_challenges/challenges/views.py
+++ Django_course/monthly_challenges/challenges/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
-# Create your views here.
+# Challenges dictionary
+monthly_challenges = {
+    "january": "Go for a run",
+    "february": "Read 20 mins",
+    "march": "Study for school",
+    "april": "Study some more!",
+    "may": "Play some tennis",
+    "june": "Go to the beach",
+    "july": "Do some coding",
+    "august": "Learn a new skill",
+    "september": "Build a videogame",
+    "october": "Get some good grades",
+    "november": "Play some sports",
+    "december": "Relax"
+}
+
+# Views Functions
+def monthly_challenge_int(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 def monthly_challenge(request, month):
-    if month == "january":
-        return HttpResponse("Go for a run!")
-    elif month == "february":
-        return HttpResponse("Read 20 mins")
-    elif month == "march":
-        return HttpResponse("Study for school")
-    else:
-        return HttpResponseNotFound("No challenges were recorded for this month")
+    try:
+        text = monthly_challenges[month]
+        return HttpResponse(text)
+    except:
+        return HttpResponseNotFound("Please enter a valid month!")
