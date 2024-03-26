c6c7dd464e2634ba859a0d7e15c7c349a4d60470
vvlaad-c <maiestrau@gmail.com>
2024-03-26 15:28:32 +0200

    Added models

--- Django_course/bookstore/book_outlet/models.py
+++ Django_course/bookstore/book_outlet/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+# Model classes
 class Book(models.Model):
+    # Fields
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+
+    # Methods
+    def __str__(self):
+       return f"{self.title} ({self.rating})"
