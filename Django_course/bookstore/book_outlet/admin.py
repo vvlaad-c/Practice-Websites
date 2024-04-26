81f0c6c1bf607be3a1d3b4cce64de2c730b76c94
vvlaad-c <maiestrau@gmail.com>
2024-04-26 17:53:08 +0300

    Finished admin panel

--- Django_course/bookstore/book_outlet/admin.py
+++ Django_course/bookstore/book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+   readonly_fields = ("slug",)
+   list_filter = ("author", "rating",)
+   list_display = ("title", "author",)
+
+admin.site.register(Book, BookAdmin)
--- Django_course/bookstore/book_outlet/models.py
+++ Django_course/bookstore/book_outlet/models.py
@@ -12,15 +12,11 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False)
+    slug = models.SlugField(default="", blank=True, editable=False, null=False)
 
     # Methods
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-    
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
     def __str__(self):
        return f"{self.title} ({self.rating})"
