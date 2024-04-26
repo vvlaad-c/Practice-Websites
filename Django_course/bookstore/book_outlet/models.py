bed3cba0af23390e213b9f9ee75d257d1df5684e
vvlaad-c <maiestrau@gmail.com>
2024-04-27 00:15:31 +0300

    Added more models

--- Django_course/bookstore/book_outlet/models.py
+++ Django_course/bookstore/book_outlet/models.py
@@ -4,15 +4,51 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Model classes
+
+class Country(models.Model):
+    # Fields
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}, {self.code}"
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+
+class Address(models.Model):
+    # Fields
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+class Author(models.Model):
+    # Fields
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 class Book(models.Model):
     # Fields
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, editable=False, null=False)
+    published_countries = models.ManyToManyField(Country)
 
     # Methods
     def get_absolute_url(self):
