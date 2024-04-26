2a78de5597a808d4438183ee5c4f6576f2081a91
vvlaad-c <maiestrau@gmail.com>
2024-04-26 17:01:46 +0300

    Finished DB and Model Work

--- Django_course/bookstore/book_outlet/models.py
+++ Django_course/bookstore/book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Model classes
 class Book(models.Model):
@@ -10,7 +12,15 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     # Methods
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def __str__(self):
        return f"{self.title} ({self.rating})"
--- Django_course/bookstore/book_outlet/views.py
+++ Django_course/bookstore/book_outlet/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Book
 
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
+
+def book_detail(request, slug):
+    try:
+        book = Book.objects.get(slug=slug)
+    except:
+        raise Http404()
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
+    
